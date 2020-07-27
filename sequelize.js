7164535ca2297a0d37c413186f70456228313130
Christellec64 <christelle.cwcs64@gmail.com>
2020-07-27 13:40:36 +0200

    deployment

--- sequelize.js
+++ sequelize.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 const { DB_NAME, DB_PASS, DB_USER, DB_DIALECT, DB_HOST } = process.env;
 
-module.exports = new Sequelize({
-  host: DB_HOST || "localhost",
-  username: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
-  dialect: DB_DIALECT,
-});
+if (process.env.DATABASE_URL) {
+  module.exports = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+  });
+} else {
+  module.exports = new Sequelize({
+    host: DB_HOST || "localhost",
+    username: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
+    dialect: DB_DIALECT,
+  });
+}
