132fdc2cba93626272f57be918206a5b10335e25
Christellec64 <christelle.cwcs64@gmail.com>
2020-07-27 13:43:46 +0200

    adding express.statis in index

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const api = require("./api");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 app.use(express.json());
 app.use(cors());
